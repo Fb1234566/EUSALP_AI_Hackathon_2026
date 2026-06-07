0af0d24a5ebaf9e4d1ab83330e5892253c85f9a7
Filippo Benedetti <filippo.benedetti3@gmail.com>
2026-06-07 23:16:33 +0200

    Added initial project

---
+++ eusalp-ai-hackathon-2026/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <ion-app>
+    <ion-split-pane when="lg" content-id="main-content">
+      <ion-menu menu-id="main-menu" side="start" content-id="main-content" type="overlay">
+        <ion-header>
+          <ion-toolbar>
+            <ion-title>Options</ion-title>
+          </ion-toolbar>
+        </ion-header>
+
+        <ion-content>
+          <ion-list lines="none">
+            <ion-menu-toggle :auto-hide="false">
+              <ion-item :detail="false" router-link="/home">Home</ion-item>
+            </ion-menu-toggle>
+            <ion-menu-toggle :auto-hide="false">
+              <ion-item :detail="false" router-link="/message/1">Sample message</ion-item>
+            </ion-menu-toggle>
+          </ion-list>
+        </ion-content>
+      </ion-menu>
+
+      <ion-router-outlet id="main-content" />
+    </ion-split-pane>
+  </ion-app>
+</template>
+
+<script setup lang="ts">
+import {
+  IonApp,
+  IonContent,
+  IonHeader,
+  IonItem,
+  IonList,
+  IonMenu,
+  IonMenuToggle,
+  IonRouterOutlet,
+  IonSplitPane,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/vue';
+</script>
---
+++ eusalp-ai-hackathon-2026/src/views/HomePage.vue
@@ -0,0 +1,41 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-title>Inbox</ion-title>
+        <ion-buttons slot="start">
+          <ion-menu-button menu="main-menu" />
+        </ion-buttons>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content :fullscreen="true">
+      <ion-list>
+        <MessageListItem v-for="message in messages" :key="message.id" :message="message" />
+      </ion-list>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import {
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonList,
+  IonMenuButton,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/vue';
+import MessageListItem from '../components/MessageListItem.vue';
+import { getMessages } from '../data/messages';
+
+const messages = getMessages();
+</script>
+
+<style scoped>
+ion-list {
+  padding-top: 0;
+}
+</style>
